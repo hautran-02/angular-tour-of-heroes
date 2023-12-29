c230330653f6aebb8b7feef8393203a8c0447fcb
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-12-29 15:06:29 +0700

    hero component step03

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heros';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, FormsModule, HeroDetailComponent],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css',
+})
+export class HeroesComponent {
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+  constructor(private heroService: HeroService) {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
